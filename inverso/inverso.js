cede16d94b11af5c2c810b6b50a45dee1c051e09
Santiago Coffey <scoffey@playdom.com>
2013-10-25 12:21:35 -0700

    Encoding and minor fixes

--- inverso/inverso.js
+++ inverso/inverso.js
@@ -204,12 +204,14 @@ $(document).ready(function () {
 	$('.googleplus').click(shareOnGooglePlus);
 
 	$('#info').click(function () { $('#about').slideToggle(500); });
+	$('#back').click(function () { $('#about').slideToggle(500); });
 	$('#reset').click(reset);
 	reset();
 });
 
 var stripAccents = (function () {
-  var in_chrs   = 'àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ',
+  var in_chrs   = '\u00e0\u00e1\u00e2\u00e3\u00e4\u00e7\u00e8\u00e9\u00ea\u00eb\u00ec\u00ed\u00ee\u00ef\u00f1\u00f2\u00f3\u00f4\u00f5\u00f6\u00f9\u00fa\u00fb\u00fc\u00fd\u00ff\u00c0\u00c1\u00c2\u00c3\u00c4\u00c7\u00c8\u00c9\u00ca\u00cb\u00cc\u00cd\u00ce\u00cf\u00d1\u00d2\u00d3\u00d4\u00d5\u00d6\u00d9\u00da\u00db\u00dc\u00dd',
+  //  in_chrs   = 'àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ',
       out_chrs  = 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY',
       chars_rgx = new RegExp('[' + in_chrs + ']', 'g'),
       transl    = {},
